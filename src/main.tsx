c43e60d3b5289badbb2c8bd2a36176ca5ea364d5
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-01-25 11:23:17 +0000

    Configure Amplify in main.tsx with project configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Amplify } from 'aws-amplify';
+import config from '../amplify_outputs.json';
+
+Amplify.configure(config);
 
 // Comment out Amplify configuration for now
 // import { Amplify } from "aws-amplify";
