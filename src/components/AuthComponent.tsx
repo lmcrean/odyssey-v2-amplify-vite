e6d38282769f391d8f59cdee6be4db6466e744e4
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-01-27 19:21:02 +0000

    refactor: remove unused useAuthenticator import

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -1,4 +1,3 @@
-import { useAuthenticator } from '../__tests__/mocks/auth/authenticator/hooks/useAuthenticator';
 import '@aws-amplify/ui-react/styles.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
