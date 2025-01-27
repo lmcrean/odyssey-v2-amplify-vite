641092e0b98cf21cb501788709b09d5bd1a391fc
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-01-27 20:31:45 +0000

    feat: add toast notifications and improve App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import AuthComponent from './components/AuthComponent';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50">
+      <ToastContainer position="top-right" />
       <AuthComponent />
     </div>
   );
