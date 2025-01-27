f9266c6c8afe6a77935e99deaf8b9a323a8430ef
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-01-27 21:02:44 +0000

    feat(router): add route for TailwindTest component at /tailwind-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import AuthComponent from './components/AuthComponent';
+import TailwindTest from './components/TailwindTest';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <ToastContainer position="top-right" />
-      <AuthComponent />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <ToastContainer position="top-right" />
+        <Routes>
+          <Route path="/" element={<AuthComponent />} />
+          <Route path="/tailwind-test" element={<TailwindTest />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
